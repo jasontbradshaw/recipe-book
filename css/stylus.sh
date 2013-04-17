083c97faa5a63a7666c013dffd688af63096ef33
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2013-04-17 18:45:40 -0500

    Initial commit

---
+++ css/stylus.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+# build and watch the stylus files
+stylus --watch --use nib --include-css --compress main.styl
---
+++ js/require.config.js
@@ -0,0 +1,5 @@
+require.config({
+  baseUrl: 'js',
+  main: 'main'
+});
+require(['main']);
