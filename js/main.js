eb23dc15c1d3023ec59c2c2b10bf982c73a78c83
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2013-04-18 12:38:16 -0500

    Write a simple getRecipe function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 define(['jquery'], function ($) {
+  // download and parse a recipe from this website
+  var getRecipe = function (name) {
+    return $.getJSON('/recipes/' + name + '.json').fail(function (xhr, err) {
+      console.error(err);
+    });
+  };
 
+  // collet all the recipes
+  getRecipe('chicken-enchiladas').done(function (recipe) {
+    console.log(recipe);
+  });
 });
