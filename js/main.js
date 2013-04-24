eabd5360b851f1c678c4ce216414b2abb6bd3a5f
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2013-04-23 20:01:56 -0500

    Refactor

--- js/main.js
+++ js/main.js
@@ -6,15 +6,18 @@ define([
   'm!templates/recipe.html'
 ], function ($, inflection, moment, recipes, tmplRecipe) {
 
+  // create a recipe jQuery DOM object, binding appropriate events
   var buildRecipe = function (recipe) {
     var $recipe = $(tmplRecipe(recipe));
 
-    // pluralize necessary amounts
+    // pluralize amounts as necessary
     $recipe.find('.measure').each(function () {
       var $type = $(this).find('.type');
       var $value = $(this).find('.value');
 
-      if (parseInt($value.text(), 10) !== 1 && $type.text() !== '' && $type.text() !== '') {
+      // pluralize only non-one values that have a type (i.e. aren't a count)
+      var value = parseInt($value.text(), 10);
+      if (value !== 1 && $type.text() !== '') {
         $type.text(inflection.pluralize($type.text()));
       }
     });
