90ed170d0f15bfaf38a72dc726dcd9daf6ac8a1e
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2013-04-25 00:17:03 -0500

    Add search result creation

--- js/main.js
+++ js/main.js
@@ -4,12 +4,26 @@ define([
   'moment',
   'search',
   'recipes',
-  'm!templates/recipe.html'
-], function ($, inflection, moment, search, recipes, tmplRecipe) {
+  'm!templates/recipe.html',
+  'm!templates/recipe-search-result.html'
+], function (
+  $,
+  inflection,
+  moment,
+  search,
+  recipes,
+  tmplRecipe,
+  tmplRecipeSearchResult
+) {
 
   // defer a function call until the stack is clear
   var defer = function (f) {
     setTimeout(function () { f(); }, 0);
+  };
+
+  // escape a string for regular expression input
+  var regexpEscape = function (s) {
+    return s.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&");
   };
 
   // create a recipe jQuery DOM object, binding appropriate events
@@ -61,25 +75,56 @@ define([
     return $recipe;
   };
 
-  // add the recipes to the search index (in the background after initial load)
-  $.each(recipes, function (i, recipe) {
-    // use the array index as the recipe's id so we can get it later
-    defer(function () { search.index.add(search.recipeToIndex(i, recipe)); });
-  });
+  var buildRecipeSearchResult = function (query, recipe) {
+    // template the recipe search result. its expected that the recipe has had
+    // an id assigned to it by this point.
+    var $result = $(tmplRecipeSearchResult(recipe));
+
+    // get all the query terms as a list
+    var terms = [];
+    var queryTerms = query.split(/\s+/);
+    for (var i = 0; i < queryTerms.length; i++) {
+      var term = queryTerms[i];
+      if (term) {
+        terms.push(regexpEscape(term));
+      }
+    }
+
+    // surround words in the result that appear in the query with a span
+    var re = new RegExp('(' + terms.join('|') + ')', 'ig');
+    $result.find('*').text(function () {
+      var text = $(this).text();
+      $(this).html(text.replace(re, '<span class="special">$&</span>'));
+    });
+
+    return $result;
+  };
 
   // set up the search bar
   var $search = $('#search input');
+  var $results = $('#results');
+
   var loadingClass = 'loading';
   var delay = 350;
   var searchTimeout = null;
 
   // update the search results with a new query
-  var getSearchResults = function (q) {
-    var results = $.map(search.index.search(q) || [], function (result) {
+  var processSearchResults = function (query) {
+    var results = $.map(search.index.search(query) || [], function (result) {
       // get each recipe by its index
-      return recipes[parseInt(result.ref, 10)];
+      var id = parseInt(result.ref, 10);
+      return $.extend({ id: id }, recipes[id]);
     });
 
+    // replace the results in the DOM with the new results
+    $results.empty();
+    $.each(results, function (i, recipe) {
+      defer(function () {
+        $results.append(buildRecipeSearchResult(query, recipe));
+      });
+    });
+
+    // we're done loading results once the search has returned
     $search.removeClass(loadingClass);
   };
 
@@ -94,10 +139,16 @@ define([
     var query = $search.val();
     if (e.keyCode === 13) {
       // start searching immediately if return is pressed
-      defer(function () { getSearchResults(query); });
+      defer(function () { processSearchResults(query); });
     } else {
       // start the timer to search for matching results
-      searchTimeout = setTimeout(function () { getSearchResults(query); }, delay);
+      searchTimeout = setTimeout(function () { processSearchResults(query); }, delay);
     }
+  });
+
+  // add the recipes to the search index (in the background after initial load)
+  $.each(recipes, function (i, recipe) {
+    // use the array index as the recipe's id so we can get it later
+    defer(function () { search.index.add(search.recipeToIndex(i, recipe)); });
   });
 });
